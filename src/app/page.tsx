432507f51dc671bb746417c4df2016fdfd560608
Anugrah Ilhami Rizki <anugrahilhamirizki@gmail.com>
2025-10-27 23:49:57 +0700

    feat(homepage): add responsiveness to hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,24 +5,23 @@ export default function Home() {
   return (
     <main className="min-h-screen">
       {/* Hero Section */}
-      <section className="flex flex-col md:flex-row items-center justify-between min-h-screen max-w-6xl mx-auto px-6">
+      <section className="flex flex-col md:flex-row items-center justify-center lg:justify-between min-h-[80vh] lg:min-h-[90vh] max-w-6xl mx-auto px-6">
         {/* Left Side Text */}
         <div className="space-y-4">
-          <h1 className="text-6xl font-bold">Hi</h1>
-          <h1 className="text-6xl font-bold">I'm {siteData.author.nickName}</h1>
-          <h2 className="text-4xl font-semibold">{siteData.author.role}</h2>
+          <h1 className="text-4xl lg:text-6xl font-bold">Hi</h1>
+          <h1 className="text-4xl lg:text-6xl font-bold">I'm {siteData.author.nickName}</h1>
+          <h2 className="text-3xl lg:text-4xl font-semibold">{siteData.author.role}</h2>
         </div>
 
         {/* Right Side Image */}
         <div
-          className="mt-10 md:mt-0 relative w-[400px] h-[400px] bg-cover bg-center rounded-lg"
+          className="flex justofy-center items-center mt-10 md:mt-0 relative w-[300px] lg:w-[400px] h-[300px] lg:h-[400px] bg-cover bg-center rounded-lg"
           style={{ backgroundImage: "url('/profile-background.png')" }}
         >
           <Image
             src={siteData.author.avatar}
             alt="Anugrah"
-            width={400}
-            height={400}
+            fill
             className="object-cover rounded-full shadow-lg"
           />
         </div>
@@ -31,7 +30,7 @@ export default function Home() {
       {/* About Me Section */}
       <section
         id="about"
-        className="py-15 max-w-4xl mx-auto px-6 text-left"
+        className="py-5 max-w-4xl mx-auto px-6 text-left mt-10 lg:mt-0"
       >
         <h2 className="text-5xl font-bold mb-6">About Me</h2>
         <p className="text-lg leading-relaxed text-gray-300">{siteData.author.bio}</p>
